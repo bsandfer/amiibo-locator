1f715319ca0f9882e9ee54aef3c71c89acd3d548
Sharon Yoon <sharonjyoon@gmail.com>
2022-03-02 14:02:34 -0800

    got rid of JS folder and just attempted to link navbascript as its own .js file.

---
+++ navbarscript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+// $(document).ready(function () {
+//   //Initialize dropdown
+//   $(".dropdown-trigger").dropdown({ hover: false });
+// });
