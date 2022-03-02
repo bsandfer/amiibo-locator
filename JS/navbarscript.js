d42f66ba55ff767356eed48bb9dfad0391630b81
Sharon Yoon <sharonjyoon@gmail.com>
2022-03-02 13:28:16 -0800

    initialize the navbar dropdown

--- JS/navbarscript.js
+++ JS/navbarscript.js
@@ -1,7 +1,7 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   var elems = document.querySelectorAll('.dropdown-trigger');
-//   var instances = M.Dropdown.init(elems, options);
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
 
 $(document).ready(function () {
   //Initialize dropdown
