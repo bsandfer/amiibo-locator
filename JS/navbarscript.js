6439c9b959191c12c991db295497ba966dcfc437
Sharon Yoon <sharonjyoon@gmail.com>
2022-03-02 13:53:46 -0800

    added navbarscript.js

--- JS/navbarscript.js
+++ JS/navbarscript.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems, options);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   var elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems);
+// });
 
 $(document).ready(function () {
   //Initialize dropdown
-  $(".dropdown-trigger").dropdown();
+  $(".dropdown-trigger").dropdown({hover: false});
 });
