fc575c2d12c63f543681b02b2c39349632205e9a
Hye Won Park <hyewonpark26@gmail.com>
2022-03-03 12:41:22 -0800

    adds detailed comments to hpscript and amiibohtml

--- hpscript.js
+++ hpscript.js
@@ -7,41 +7,38 @@ fetch(amiiboUrl)
   })
   .then(function (data) {
     //looping over the fetch response and inserting the URL of your repos into a list
-    console.log(data);
-    console.log(data.amiibo[0]);
-    console.log(data.amiibo[0].release);
     for (let i = 0; i < data.amiibo.length; i++) {
 
-      //amiiboSeries character image  type release {au:, eu: jp na}
 
       let list = document.createElement('li');
       let divOne = document.createElement('div'); //for the collapsible header
       let divTwo = document.createElement('div'); //for the collapsible body
 
-      //header of the collapsible, presenting character name of amiibo
+      //header of the collapsible, presenting character name and image of amiibo
       divOne.classList.add('collapsible-header');
       divOne.textContent = data.amiibo[i].character;
-
-      //body of collapsible, presenting image, type, amiibo series info 
-      divTwo.classList.add('collapsible-body');
-
       let img = document.createElement("img");
       img.src = data.amiibo[i].image;
       img.classList.add('imgClass');
       divOne.appendChild(img);
 
 
+
+      //body of collapsible, presenting game series, amiibo series, type, and release info
+      divTwo.classList.add('collapsible-body');
+
       let info = document.createElement('ul');
 
+      // game series
       let info_El0 = document.createElement('li');
       info_El0.textContent = "GAME SERIES: " + data.amiibo[i].gameSeries;
-
+      // amiibo series
       let info_El1 = document.createElement('li');
       info_El1.textContent = "AMIIBO SERIES: " + data.amiibo[i].amiiboSeries
-
+      //type of amiibo
       let info_El2 = document.createElement('li');
       info_El2.textContent = "TYPE: " + data.amiibo[i].type;
-
+      //amiibo release info
       let info_El3 = document.createElement('li');
       info_El3.textContent = "RELEASE: "
       //for each of the release continents
@@ -55,7 +52,7 @@ fetch(amiiboUrl)
       info_El3au.textContent = "Australia: " + data.amiibo[i].release.au
 
 
-
+      //appending all the info elements to the main info which is then appended to collapsible body(divTwo)
       info.append(info_El0);
       info.append(info_El1);
       info.append(info_El2);
